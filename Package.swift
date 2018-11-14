2f7b1a3ce1e97933372ac966c45f8d08599b50e5
doofyus <mikk.ratsep@gmail.com>
2018-11-14 11:27:44 +0100

    updated Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/copenssl", .branch("master")),
-        .package(url: "https://github.com/highmobility/hm-utilities-swift", .branch("master")),
+        .package(url: "https://github.com/highmobility/hm-utilities-swift", .upToNextMinor(from: "1.2.0")),
     ],
     targets: [
         .target(name: "HMCryptoKit", dependencies: ["HMUtilities"]),
