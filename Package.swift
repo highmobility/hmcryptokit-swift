68062eb02fa6a608850015bf360512dae3f7938e
doofyus <mikk.ratsep@gmail.com>
2019-08-20 14:56:41 +0200

    updated swift-tools-version

--- Package.swift
+++ Package.swift
@@ -1,27 +1,27 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "HMCryptoKit",
+    platforms: [
+        .iOS(.v10),
+        .macOS(.v10_10),
+        .tvOS(.v10)
+    ],
     products: [
         .library(name: "HMCryptoKit", targets: ["HMCryptoKit"]),
         .library(name: "HMCryptoKitCommandsInfo", targets: ["HMCryptoKitCommandsInfo"]),
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/copenssl", .exact("1.0.0-rc.1")),
-        .package(url: "https://github.com/highmobility/hm-utilities-swift", .upToNextMinor(from: "1.4.0")),
+        .package(url: "https://github.com/highmobility/hm-utilities-swift", .upToNextMinor(from: "1.4.1")),
     ],
     targets: [
         .target(name: "HMCryptoKit", dependencies: ["HMUtilities"]),
         .target(name: "HMCryptoKitCommandsInfo", dependencies: ["HMCryptoKit"]),
         .target(name: "HMCryptoKitCLT", dependencies: ["HMCryptoKit", "HMCryptoKitCommandsInfo"]),
         .testTarget(name: "HMCryptoKitTests", dependencies: ["HMCryptoKit"]),
-    ],
-    platforms: [
-        .iOS(.v10),
-        .macOS(.v10_10),
-        .tvOS(.v10)
     ]
 )
